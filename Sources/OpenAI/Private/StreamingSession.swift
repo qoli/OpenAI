3b74c8b5cba8ec1a3b3905a8136bebdb4d0288bb
Ronnie W <llqoli@gmail.com>
2025-02-28 16:25:05 +0800

    優化 StreamingSession 類別，簡化程式碼並改善 JSON 處理邏輯

--- Sources/OpenAI/Private/StreamingSession.swift
+++ Sources/OpenAI/Private/StreamingSession.swift
@@ -1,49 +1,48 @@
 //
 //  StreamingSession.swift
-//  
+//
 //
 //  Created by Sergii Kryvoblotskyi on 18/04/2023.
 //
 
 import Foundation
 #if canImport(FoundationNetworking)
-import FoundationNetworking
+    import FoundationNetworking
 #endif
 
 final class StreamingSession<ResultType: Codable>: NSObject, Identifiable, URLSessionDelegate, URLSessionDataDelegate {
-    
     enum StreamingError: Error {
         case unknownContent
         case emptyContent
     }
-    
+
     var onReceiveContent: ((StreamingSession, ResultType) -> Void)?
     var onProcessingError: ((StreamingSession, Error) -> Void)?
     var onComplete: ((StreamingSession, Error?) -> Void)?
-    
+
     private let streamingCompletionMarker = "[DONE]"
     private let urlRequest: URLRequest
     private lazy var urlSession: URLSession = {
         let session = URLSession(configuration: .default, delegate: self, delegateQueue: nil)
         return session
     }()
-    
+
     private var previousChunkBuffer = ""
 
     init(urlRequest: URLRequest) {
         self.urlRequest = urlRequest
     }
-    
+
     func perform() {
-        self.urlSession
-            .dataTask(with: self.urlRequest)
+        urlSession
+            .dataTask(with: urlRequest)
             .resume()
     }
-    
+
     func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
         onComplete?(self, error)
     }
-    
+
     func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
         guard let stringContent = String(data: data, encoding: .utf8) else {
             onProcessingError?(self, StreamingError.unknownContent)
@@ -51,15 +50,18 @@ final class StreamingSession<ResultType: Codable>: NSObject, Identifiable, URLSe
         }
         processJSON(from: stringContent)
     }
-    
 }
 
 extension StreamingSession {
-    
     private func processJSON(from stringContent: String) {
         if stringContent.isEmpty {
             return
         }
+
+        if stringContent.hasPrefix("data:") {
+            return
+        }
+
         let jsonObjects = "\(previousChunkBuffer)\(stringContent)"
             .trimmingCharacters(in: .whitespacesAndNewlines)
             .components(separatedBy: "data:")
@@ -67,11 +69,11 @@ extension StreamingSession {
             .filter { $0.isEmpty == false }
 
         previousChunkBuffer = ""
-        
+
         guard jsonObjects.isEmpty == false, jsonObjects.first != streamingCompletionMarker else {
             return
         }
-        jsonObjects.enumerated().forEach { (index, jsonContent)  in
+        jsonObjects.enumerated().forEach { index, jsonContent in
             guard jsonContent != streamingCompletionMarker && !jsonContent.isEmpty else {
                 return
             }
@@ -94,5 +96,4 @@ extension StreamingSession {
             }
         }
     }
-    
 }
